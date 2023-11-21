7667ab773c3d1d627a070f35b8f324fc0708d15a
jeromsanjeevan <jerom6301@gmail.com>
2023-11-21 17:25:44 +0000

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: false })], 
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
