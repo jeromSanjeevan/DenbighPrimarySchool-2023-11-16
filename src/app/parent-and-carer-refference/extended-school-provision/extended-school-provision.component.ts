bfeaad62b91a2fa11995745a095b7672a11a6ec3
Jerom <jerom6301@gmail.com>
2023-11-23 12:38:39 +0000

    22-11-2023

--- src/app/parent-and-carer-refference/extended-school-provision/extended-school-provision.component.ts
+++ src/app/parent-and-carer-refference/extended-school-provision/extended-school-provision.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+export interface YourItem {
+  id: number;
+  name: string;
+  description: string;
+  // Add other properties as needed
+}
 
 @Component({
   selector: 'app-extended-school-provision',
@@ -7,4 +15,23 @@ import { Component } from '@angular/core';
 })
 export class ExtendedSchoolProvisionComponent {
   headerHeading: string = 'Extended School Provision';
+  items: YourItem[] = [];
+
+  ngOnInit(): void {
+    this.getItems().subscribe(items => {
+      this.items = items;
+    });
+  }
+  getItems(): Observable<YourItem[]> {
+    const items: YourItem[] = [
+      { id: 1, name: 'Item 1', description: 'Description for Item 1' },
+      { id: 2, name: 'Item 2', description: 'Description for Item 2' },
+      { id: 3, name: 'Item 3', description: 'Description for Item 3' },
+      // Add more items as needed
+    ];
+
+    return of(items);
+  }
+
+
 }
