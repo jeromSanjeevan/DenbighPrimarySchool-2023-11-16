fc75c94d272a11f2dadf46908de4af95a01caecf
Jerom <jerom6301@gmail.com>
2024-01-04 15:42:06 +0000

    1

--- src/app/curriculum-year-groups/year-four/year-four.component.ts
+++ src/app/curriculum-year-groups/year-four/year-four.component.ts
@@ -9,7 +9,7 @@ export class YearFourComponent {
   Open_Y4_Brocher_Spring() {
     // Open the PDF link in a new tab
     window.open(
-      'https://drive.google.com/file/d/1MbKMAjDaJaM0HQtbKStjufT-iz6LJvyU/view?usp=sharing',
+      'https://drive.google.com/file/d/1MbKMAjDaJaM0HQtbKStjufT-iz6LJvyU/view',
       '_blank'
     );
   }
--- src/app/curriculum-year-groups/year-two/year-two.component.ts
+++ src/app/curriculum-year-groups/year-two/year-two.component.ts
@@ -9,7 +9,7 @@ export class YearTwoComponent {
   Open_Y2_Brocher_Spring() {
     // Open the PDF link in a new tab
     window.open(
-      'https://drive.google.com/file/d/1RF1nYjF8ktVViR6jhe8vdsV_KYR815sp/view?usp=drive_link',
+      'https://drive.google.com/file/d/1bh26hu9S7F2gR9JDQmDmwFM8XBwudgfa/view',
       '_blank'
     );
   }
