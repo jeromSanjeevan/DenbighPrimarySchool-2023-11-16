9dcc8a732c957a746c7fd8b979adf96f6557dbb9
Jerom <jerom6301@gmail.com>
2024-01-04 15:49:04 +0000

    1

--- src/app/curriculum-year-groups/year-two/year-two.component.ts
+++ src/app/curriculum-year-groups/year-two/year-two.component.ts
@@ -9,7 +9,7 @@ export class YearTwoComponent {
   Open_Y2_Brocher_Spring() {
     // Open the PDF link in a new tab
     window.open(
-      'https://drive.google.com/file/d/1bh26hu9S7F2gR9JDQmDmwFM8XBwudgfa/view',
+      'https://drive.google.com/file/d/1W5QAewwixYmPFdVr1RFpVNep2xZwUHmY/view',
       '_blank'
     );
   }
